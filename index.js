7ca396f5c85247c4040f0cb0373c412d85f08918
nekkiyy <143201886+nekkiyy@users.noreply.github.com>
2023-09-16 11:00:02 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  });
+  function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  document.querySelector(".burger-lines").addEventListener('click',() => {
+    document.querySelector(".burger-menu").classList.toggle("active")
+})
